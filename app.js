f178656be6d605e507aaffa5c566cc24eaeb0490
Brendan Woodell <brendan.woodell@gmail.com>
2018-11-29 16:29:33 -0800

    wtf happened in app.js?

--- app.js
+++ app.js
@@ -14,15 +14,6 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 const Routes = require('./src/routes/route.js')
 app.use('/', Routes)
 
-return axios(`http://localhost:3000${path}`, {
-  method: method,
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  },
-  data: body
-})
-
 app.use((err, req, res, next) => {
   console.error(err)
   const status = err.status || 500
@@ -37,3 +28,7 @@ if (process.env.NODE_ENV !== 'development') {
     const listener = () => console.log(`Spielberg is on ${port}`)
     let server = app.listen(port, listener)
 }
+
+
+// https://arcane-brook-34722.herokuapp.com/
+// https://git.heroku.com/arcane-brook-34722.git
